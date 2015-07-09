822840a8420275bdec8671bc4812d1667b6077e9
Paul Soladoye <psoladoye@gmail.com>
2015-07-09 15:46:26 -0400

    Got the clear completed working perfectly

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -5,22 +5,29 @@ export default Ember.Component.extend({
 		createTodo: function(newTitle){
 			this.set('newTitle', '');
 			this.sendAction('createTodo', newTitle);
+		},
+		clearCompleted: function(){
+			var completed = this.get('model').filterBy('isCompleted', true);
+			completed.invoke('destroyRecord');
+			console.log("Task has been deleted");
 		}
 	},
 	remaining: (function () {
 		var model = this.get('model');
 		return model.filterBy('isCompleted', false).get('length');
 	}).property('model.@each.isCompleted'),
-	// remaining: Ember.computed('model.@each.isCompleted', function(){
-	// 	var model = this.get('model');
-	// 	return model.filterBy('isCompleted', false).get('length');
-	// }),
-	// inflection: Ember.computed('remaining', function(){
-	// 	var remaining = this.get('remaining');
-	// 	return (remaining === 1) ? 'item' : 'items';
-	// })
-	inflection: (function(){
+
+	inflection: (function() {
 		var remaining = this.get('remaining');
 		return (remaining === 1) ? 'item' : 'items';
-	}).property('remaining')
+	}).property('remaining'),
+
+	hasCompleted: (function() {
+		return this.get('completed') > 0;
+	}).property('completed'),
+
+	completed: (function() {
+		var model = this.get('model');
+		return model.filterBy('isCompleted', true).get('length');
+	}).property('model.@each.isCompleted')
 });
